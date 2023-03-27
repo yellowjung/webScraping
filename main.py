cf145e57cd9c863cbef9d445890c0328c3b4e69a
YoungdonJung <yeavov@gmail.com>
2023-03-27 01:47:57 +0000

    Get job posts and delete not necessary tag

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ search_term = "python"
 
 response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
-    print("Can't request website")
+  print("Can't request website")
 else:
-    soup = BeautifulSoup(response.text, "html.parser")
-    jobs = soup.find_all('section', class_="jobs")
+  soup = BeautifulSoup(response.text, "html.parser")
+  jobs = soup.find_all('section', class_="jobs")
+  for job_section in jobs:
+    job_posts = job_section.find_all('li')
+    job_posts.pop(-1)
+    for post in job_posts:
+      print(post)
+      print("///////////////////////////////")
